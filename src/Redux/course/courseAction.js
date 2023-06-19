df56b5480f761ae6f0ce70ca973782ba0aadd5ac
maqsood-hashim <136849315+maqsood-hashim@users.noreply.github.com>
2023-06-19 01:30:23 -0700

    Update courseAction.js

--- src/Redux/course/courseAction.js
+++ src/Redux/course/courseAction.js
@@ -10,7 +10,7 @@ export const getCourses = (courseInfo) => {
 
 export const fetchCourseInfo = () => {
   return (dispatch) => {
-    Axios.get("/get-courses", {
+    Axios.get("https://kind-blue-coral-hose.cyclic.app/get-courses", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("auth_token"),
       },
@@ -29,7 +29,7 @@ export const fetchCourseInfo = () => {
 export const deleteCourseItem = (courseId) => {
   return (dispatch) => {
     try {
-        fetch("/delete", {
+        fetch("https://kind-blue-coral-hose.cyclic.app/delete", {
             method: "delete",
             headers: {
               Authorization: "Bearer " + localStorage.getItem("auth_token"),
