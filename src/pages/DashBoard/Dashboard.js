0830d195341302f5be81cd4588ebe5703b832f36
maqsood-hashim <136849315+maqsood-hashim@users.noreply.github.com>
2023-06-19 04:53:33 -0700

    Update Dashboard.js

--- src/pages/DashBoard/Dashboard.js
+++ src/pages/DashBoard/Dashboard.js
@@ -62,7 +62,7 @@ const Dashboard = () => {
         <Sidebar Icon={PersonIcon} title="Profile" link="/profile" />
         <Sidebar Icon={Grade} title="Grades" link="/grades" />
         
-        <Sidebar Icon={TouchAppIcon} title="Course" link="/all-courses" />
+        // <Sidebar Icon={TouchAppIcon} title="Course" link="/all-courses" />
         <Sidebar Icon={ExitToAppIcon} title="Logout" />
    
       </div>
